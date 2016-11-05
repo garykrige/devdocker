d494e348040b1d8f99e520da926b97b27bd18c12
Gary <garykrige@gmail.com>
2016-11-05 21:39:10 +0200

    Change to https

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,15 @@ chmod 600 .ssh/authorized_keys
 
 sed -i "s/#AuthorizedKeysFile/AuthorizedKeysFile/" /etc/ssh/sshd_config
 service ssh start
- 
-git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/superbalist/cms.superbalist.com.git
-git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/superbalist/superbalist-kubernetes.git
+
+git clone https://${GITHUB_TOKEN}@github.com/superbalist/cms.superbalist.com.git
+git clone https://${GITHUB_TOKEN}@github.com/superbalist/superbalist-kubernetes.git
 
 cd superbalist-kubernetes
+git submodule init
+sed -i "s#git@github.com#https://${GITHUB_TOKEN}@github.com#g" .git/config
+sed -i "s#github.com:Superbalist#github.com/Superbalist#g" .git/config
+
 git submodule update --recursive
 
 git config --global credential.helper cache
