9fa1658161d7cbe0a5af73cb56c5a95f08e72093
Gary <garykrige@gmail.com>
2016-11-09 10:13:25 +0200

    Specify correct folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,11 +31,11 @@ git config --global push.default matching
 # local cluster config
 kubectl config set-cluster minikube \
     --server=https://10.3.0.1 \
-    --certificate-authority=~/.secret/ca.pem
+    --certificate-authority=.secret/ca.pem
 kubectl config set-credentials default-admin \
-    --certificate-authority=~/.secret/ca.pem \
-    --client-key=~/.secret/admin-key.pem \
-    --client-certificate=~/.secret/admin.pem
+    --certificate-authority=.secret/ca.pem \
+    --client-key=.secret/admin-key.pem \
+    --client-certificate=.secret/admin.pem
 kubectl config set-context minikube \
     --cluster=minikube \
     --user=default-admin
